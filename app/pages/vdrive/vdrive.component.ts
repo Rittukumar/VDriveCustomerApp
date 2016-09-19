1b284bea69bd1ad5c3091d952231cf0efac23a81
atmohsin <atmohsin@gmail.com>
2016-09-20 00:46:22 +0530

    changes

--- app/pages/vdrive/vdrive.component.ts
+++ app/pages/vdrive/vdrive.component.ts
@@ -1,13 +1,39 @@
-import {Component} from '@angular/core';
+import {Component, ChangeDetectionStrategy, ElementRef } from '@angular/core';
+import {SetupItemViewArgs} from "nativescript-angular/directives";
+
+class Item {
+    constructor(public name: string) { }
+}
+
+var items = ["ALL Heroes (header)", "Razor", "Rubick", "Phantom Lancer", "Legion Commander", "Brewmaster", "Outworld Devourer",
+"Sniper", "Lina", "Sven", "Visage","Undying", "Tiny", "Tidehunter", "Puck", "Ursa", 
+"Magnus", "Earthshaker", "Windrunner", "Techies", "Crystal Maiden","Batrider", "Riki", "Invoker", "Venomancer", 
+"Timbersaw","Wraithking", "Anti Mage", "Ancient Apparition", "Troll Warlord", "Lich", "Enchantress", "Bristleback", "Pudge", "(footer)"];
+
 
 @Component({
 	selector: 'vdrive-component',
-    templateUrl: "pages/vdrive/vdrive.html" 
+    styleUrls:["pages/vdrive/vdrive-component.css"],
+    templateUrl: "pages/vdrive/vdrive.html" ,
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class VdriveComponent {
-    public selectedIndex: number;
+
+    public dataItems: Array<Item>;
+
+    public checked: boolean = false;
     
-    constructor() {
-        this.selectedIndex = 1;
+    constructor(public elementRef: ElementRef) {
+        this.dataItems = [];
+
+        for (var i = 0; i < items.length; i++) {
+            this.dataItems.push(new Item(items[i]));
+        }
+    }
+
+    onSetupItemView(args: SetupItemViewArgs) { 
+        args.view.context.third = (args.index % 3 === 0);
+        args.view.context.header = ((args.index + 1) % items.length === 1);
+        args.view.context.footer = (args.index + 1 === items.length);
     }
 }
